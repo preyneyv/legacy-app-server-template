d921e6792826ba61a480975e05794ebc61141801
Pranav Nutalapati <pranavnutalapati@gmail.com>
2018-04-04 22:14:45 +0530

    First commit

---
+++ admin/index.js
@@ -0,0 +1,12 @@
+const app = require('express')()
+
+// This is the admin server
+// It will be accessible at
+// http://<app server url>/admin/<app url>
+// This will happen post authentication
+// So your app doesn't have to worry about that
+
+
+function init() {
+	// Initialize the admin server here
+}
